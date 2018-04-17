541bb1f08b4f60d06e15b169a55f7031bb5bc5e7
Mike Donnelly <sqldonnelly@gmail.com>
2018-04-17 22:56:31 +0900

    Added logic for reacing file

--- populatron.js
+++ populatron.js
@@ -1,4 +1,25 @@
 const fs = require("fs");
+const parse = require("csv-parse");
+
+const totalPopulation = (inputPath) => {
+  fs.readFile(inputPath, "utf8", function(err, data) {
+    if (err) {
+      return 0;
+    }
+    parse(data, { comment: "#" }, function(err, output) {
+      if (err) {
+        return 0;
+      }
+      let population = 0;
+      for (let i = 1; i < output.length; i++) {
+        population += Number.parseInt(output[i][2]);
+      }
+      return population;
+    });
+  });
+};
+
+totalPopulation("./cities.csv").onFinished;
 
 module.exports = {
   totalPopulation(onFinished) {},
