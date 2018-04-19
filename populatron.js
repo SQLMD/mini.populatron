e4dd39b5f597b9db9a5a8304eec74514ca6a1ff9
Mike Donnelly <sqldonnelly@gmail.com>
2018-04-19 14:50:12 +0900

    Changed callback name back to onFinished and cleaned up comments

--- populatron.js
+++ populatron.js
@@ -2,9 +2,7 @@ const fs = require("fs");
 const parse = require("csv-parse");
 
 module.exports = {
-  //added a inputPath paramter because it made it easier for me to see that I was
-  //passing paramters from the test.js
-  totalPopulation(callback, inputPath = "./cities.csv") {
+  totalPopulation(onFinished, inputPath = "./cities.csv") {
     fs.readFile(inputPath, "utf8", function(err, data) {
       if (err) {
         return 0;
@@ -17,9 +15,7 @@ module.exports = {
         for (let i = 1; i < output.length; i++) {
           population += Number.parseInt(output[i][2]);
         }
-        //changed onFinished to callback to remove some of my confusion
-        //this line was the hardest to figure out
-        callback(population);
+        onFinished(population);
       });
     });
   },
--- test/test.js
+++ test/test.js
@@ -1,15 +1,12 @@
 const populatron = require("../populatron");
 const { expect } = require("chai");
+// Setup
+const expected = 561453504;
 
 describe("populatron", () => {
   it("should find total population with an inputPath parameter", (done) => {
-    // Setup
-    const expected = 561453504;
-    // Exercise
-    // Moved callback to a named function so I could conceptualize better
     const test = (actual) => {
       expect(actual).to.equal(expected);
-      //Teaf Down
       done();
     };
 
@@ -17,8 +14,6 @@ describe("populatron", () => {
   });
 
   it("should find total population", (done) => {
-    // Setup
-    const expected = 561453504;
     // Exercise
     populatron.totalPopulation((actual) => {
       // Assert
